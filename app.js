9986b824137bfbf11b631bd24ea2dbcfdbfa84ae
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2023-05-06 15:03:32 +0900

    画面遷移を適正化
    Web サーバーが使えるようになったので、 POST メソッドを使ったフォーム送信やリダイレクト処理ができるようになった。
    
    テンプレートとエンドポイントを修正し、正しい遷移先の URL および HTTP メソッドを使うよう修正する。
    
    ただし、ログイン関連処理は未実装なため、ログイン状態に応じた条件分岐に対応できない。そのため、内部処理はいったんスタブとして、正常系の動きを模倣させる。

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ const port = 3000;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  res.redirect("/chat");
+});
+
+app.get("/logout", (req, res) => {
+  res.redirect("/");
 });
 
 app.get("/chat", (req, res) => {
   const randomNickname = Math.random().toString(32).substring(2);
   res.render("chat", { session: { nickname: randomNickname } });
+});
+
+app.post("/chat", (req, res) => {
+  res.redirect("/chat");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
