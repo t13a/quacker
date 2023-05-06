95fa136d86b9da3d3b39516784b278994a78fce2
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2023-05-06 14:41:02 +0900

    動的なページを表示する
    テンプレートエンジン (EJS) を導入し、アクセス毎にページの内容を変化できるようにする。
    
    各画面のエンドポイント (/login, /chat) を追加し、テンプレートとなる HTML を views ディレクトリに移動する。これにより http://localhost:3000/login などでログイン画面を表示できるようになる。この時点ではまだ動的な要素を実装していない。

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
