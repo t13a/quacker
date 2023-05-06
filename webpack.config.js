b1b8a320e415282070bb5684bedc5ff0ace89b39
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2023-05-06 19:42:00 +0900

    Webpack を導入
    Webpack はモジュールバンドラーの一種で、現代的な Web 開発スタックの多くはこれを要求する。モジュールバンドラーの本来の役割は複数の JavaScript などをひとつのファイルに結合・軽量化してパフォーマンスを改善することにあるが、主たる用途は結合時の変換処理にある。これにより各種トランスパイラなどが併用可能になるため、現代の Web 開発で必要不可欠な存在となっている。
    
    既定では JavaScript の結合だけを行うため、 HTML の記述を変更し、バンドルファイルの生成先のパス (*.bundle.js) を指定する。
    
    また、バンドルファイルを Git のコミット対象から除外する。
    
    現時点ではバンドルファイルの生成は手動 (webpack コマンド) で行う必要があり、単一の JavaScript しか使用していないため機能的なメリットはない (むしろ面倒が増えるだけである)。

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "source-map",
+  mode: "development",
+  entry: {
+    chat: "./public/js/chat.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/js/",
+  },
+};
